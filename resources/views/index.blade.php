9683befb0501432a6ffb21cf7497b5cc3c44221f
黃河濤 <3B032081@gm.student.ncut.edu.tw>
2024-12-05 20:50:16 +0800

    修改子樣板post.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
                         <div class="site-heading">
                             <h1>Clean Blog</h1>
                             <span class="subheading">A Blog Theme by Start Bootstrap</span>
-                            
+
                         </div>
                     </div>
                 </div>
